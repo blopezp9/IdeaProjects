df1daa999c7a7235f7e193b274427bb0ffd8cab4
blopezp9 <blopezp@eafit.edu.co>
2021-09-11 01:23:35 -0500

    config bootstrap.yml v5

--- Movies/src/main/java/cloud/trabajo/MoviesApplication.java
+++ Movies/src/main/java/cloud/trabajo/MoviesApplication.java
@@ -2,8 +2,10 @@ package cloud.trabajo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MoviesApplication {
 
     public static void main(String[] args) {
